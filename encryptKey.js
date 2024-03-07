4b5544c155ed3bd6b657e1c645bfe21be3999489
Ayodeji Komolafe <kvngeko@icloud.com>
2024-03-07 07:22:46 -0500

    prettier formatting

--- encryptKey.js
+++ encryptKey.js
@@ -3,15 +3,12 @@ const fs = require("fs-extra");
 require("dotenv").config();
 
 async function main() {
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-    const encryptedJsonKey = await wallet.encrypt(process.env.PRIVATE_KEY_PASSWORD);
-    console.log(encryptedJsonKey);
-    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
-
-
-
-
-
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
 }
 main()
   .then(() => process.exit(0))
